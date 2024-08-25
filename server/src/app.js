1c835c1b8a5f54c7f27be58ea375a5cf39ffde73
Monu <ms88006218@gmail.com>
2024-08-25 10:24:25 +0530

    centralize db connection and server initialization

--- server/src/app.js
+++ server/src/app.js
@@ -4,7 +4,33 @@ import cookieParser from 'cookie-parser'
 import cors from 'cors'
 import errorHandler from './middlewares/errorHandler.js';
 import config from './config.js';
+import connectDB from './database/index.js';
+
 const app = express();
+const port = config.port || 3000;
+
+// Connecting to mongoDB
+connectDB()
+   .then(() => {
+      if (config.node_env !== 'production') {
+         // Only run the server locally
+         app.listen(port, () => {
+            console.log(`Listening on port ${port}`);
+         });
+
+         app.get('/', (req, res) => {
+            res.send('Quizroom');
+         });
+      } else {
+         console.log('Connected to database');
+      }
+   })
+
+   .catch((error) => {
+      console.log(`Error connecting to database: ${error}`);
+   });
+
+
 
 // Middlewares
 app.use(express.json());
